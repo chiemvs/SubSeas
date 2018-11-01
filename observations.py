3081499686d7ba79ee82f2ba5d1070acccd91c2c
straaten <straaten@pc160104.knmi.nl>
2018-11-01 16:06:39 +0000

    time-aggregation works, regular spatial aggregation is an issue still.

--- observations.py
+++ observations.py
@@ -1,4 +1,7 @@
 import os
+import numpy as np
+import xarray as xr
+import pandas as pd
 
 class SurfaceObservations(object):
     
@@ -10,7 +13,7 @@ class SurfaceObservations(object):
     
     def downloadraw(self):
         """
-        Downloads highres observations on regular 0.25 degree lat-lon grid
+        Downloads daily highres observations on regular 0.25 degree lat-lon grid
         """
         import urllib3
         
@@ -42,24 +45,80 @@ class SurfaceObservations(object):
         f.close()
         os.system("gunzip -k " + zippath) # Results in file written at filepath
         
-    def loadraw(self):
+    def loadraw(self, tmin = None, tmax = None, llcrnr = (None, None), rucrnr = (None,None)):
         """
-        Creates new attributes: marray with the masked data, units, lons and lats
+        Loads the netcdf (possibly delimited by maxtime and corners). Corners need to be given as tuples (lat,lon)
+        Creates new attributes: the array with the data, units, and coordinates
         """
-        import scipy.io as sio
-        import numpy as np
+    
+        #import scipy.io as sio
         
         if not os.path.isfile(self.filepath):
             self.downloadraw()
+            
+        full = xr.open_dataarray(self.filepath)
+        if tmin is None:
+            tmin = np.min(full.coords['time'].values)
+        if tmax is None:
+            tmax = np.max(full.coords['time'].values)
+        self.array = full.sel(time = pd.date_range(tmin, tmax, freq = 'D'), longitude = slice(llcrnr[1], rucrnr[1]), latitude = slice(llcrnr[0], rucrnr[0])) # slice gives an inexact lookup, everything within the range
         
-        ifile = sio.netcdf_file(self.filepath, mode = 'r')
-        variable = ifile.variables[self.variable] # Netcdf variable object referring to file location
-        missval = variable._get_missing_value()
-        self.units = variable.units.decode()
-        self.lons = ifile.variables['longitude'].data
-        self.lats = ifile.variables['latitude'].data
-        self.marray = np.ma.masked_array(variable.data, np.equal(variable.data, missval))
-        del(variable, missval)
-        ifile.close()
+        #ifile = sio.netcdf_file(self.filepath, mode = 'r')
+        #templons = ifile.variables['longitude'].data
+        #templats = ifile.variables['latitude'].data
+        #temptime = ifile.variables['time']
         
+        
+        #variable = ifile.variables[self.variable] # Netcdf variable object referring to file location
+        #missval = variable._get_missing_value()
+        #self.units = variable.units.decode()
 
+        #self.marray = np.ma.masked_array(variable.data, np.equal(variable.data, missval))
+        #del(variable, missval)
+        #ifile.close()
+    
+    def aggregatespace(self, step, by_degree = False):
+        """
+        Regular lat lon or gridbox aggregation by creating new single coordinate which is used for grouping.
+        In the case of degree grouping the groups might not be of equal size.
+        """
+        if by_degree:
+            binlon = pd.cut(self.array.longitude, bins = np.arange(self.array.longitude.min(), self.array.longitude.max(), step))
+            binlat = pd.cut(self.array.latitude, bins = np.arange(self.array.latitude.min(), self.array.latitude.max(), step))
+        else:
+            lon_n, lon_rem = divmod(self.array.longitude.size, step)
+            binlon = np.repeat(np.arange(0, lon_n), repeats = step)
+            binlon = np.append(binlon, np.repeat(np.NaN, lon_rem))
+            lat_n, lat_rem = divmod(self.array.latitude.size, step)
+            binlat = np.repeat(np.arange(0, lat_n), repeats = step)
+            binlat = np.append(binlat, np.repeat(np.NaN, lat_rem))
+        
+        # Initialize the grouped extra variables
+        lon2 = xr.DataArray(binlon, [self.array.coords['longitude']])
+        lat2 = xr.DataArray(binlat, [self.array.coords['latitude']])
+        
+        stacked = self.array.stack()
+        
+        nl_avg_lon = self.array.groupby_bins('longitude', np.arange()).mean()
+        
+    
+    def aggregatetime(self, freq = 'w' , method = 'mean'):
+        """
+        Uses the pandas frequency indicators. Method can be mean, min, max, std
+        """
+        tempattr = self.array.attrs
+        f = getattr(self.array.resample(time = freq), method) # timestamp can be set with label = 'right'
+        self.array = f('time') # Removes the attributes.
+        tempattr['timemethod'] = '_'.join([freq,method])
+        self.array.attrs = tempattr
+        # To access days of week: self.array.time.dt.timeofday
+        # Also possible is self.array.time.dt.floor('D')
+        
+    # Define a 'save' method? possibly using the experiment name given through kwargs?   
+    # Define a detrend method.
+        
+# I want to keep the real advanced operations seperate, like PCA, clustering, 
+        
+# I want to make a class for heatevent. within the definitionmethods I want to have an option to convert the array to a flat dataframe. remove NA'. Then based on previously clustering I can also aggregate by spatial grouping?
+
+# Later on I want to make an experiment class. Combining surface observations on a certain aggregation, with 
