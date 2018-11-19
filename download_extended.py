994f19369351a4d36f9fdff88cd8f4b5b6e5c9cd
chiemvs <chiemvs@gmail.com>
2018-11-19 16:08:12 +0000

    Loop to go over GRIB files in hindcast

--- download_extended.py
+++ download_extended.py
@@ -4,9 +4,13 @@ import ecmwfapi
 #from datetime import date,timedelta
 import os
 #import gzip
-#import numpy as np
+import numpy as np
 #import sys
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
 import pandas as pd
+import xarray as xr
+import pygrib
 
 # Extended range has 50 perturbed members. and 1 control forecast. Steps: "0/to/1104/by/6"
 # Reforecasts have 11 members (stream = "enfh", number "1/to/10"). Probably some duplicates will arise.
@@ -53,9 +57,11 @@ def download_forecast(indate):
 def download_hindcast(indate):
     """
     Constructs hdates belonging to indate. For retrieval efficiency all Hdates are
-    initially downloaded together and saved under indate but recognizable by 'hind'. 
-    Then the files are separated. Separate download of perturbed (11 members) and control. 
-    Both are joined into one final file. 
+    initially downloaded together, which is only possible in a GRIB file. 
+    The single file is saved under indate but recognizable by 'hin'. 
+    Separate download of perturbed (11 members) and control. 
+    Then the hdates within a file are extracted and perturbed and control are joined.
+    The final files are saved per hdate as netcdf.
     """
 
     end = pd.to_datetime(indate, format='%Y-%m-%d')
@@ -64,8 +70,9 @@ def download_hindcast(indate):
     hdates.sort()
     marshdate = '/'.join(hdates)
     
-    pfname = 'hin_'+indate+'_ens.nc'
-    cfname = 'hin_'+indate+'_contr.nc'
+    pfname = 'hin_'+indate+'_ens.grib'
+    cfname = 'hin_'+indate+'_contr.grib'
+    #combname = 'hin_'+hadate+'_comb.nc' # needs to be defined within the loop
     
     # perturbed.
     if os.path.isfile(basedir+pfname):
@@ -78,7 +85,42 @@ def download_hindcast(indate):
     else:
         server.execute(mars_dict(indate, hdate = marshdate, contr = True), basedir+cfname)
         
-    # here loop to pull hdates in one file out of each other.
+    # here loop to pull hdates in one file out of each other. Conversion to netcdf with 3 parameters.
+    # Within grib file: first per variable, than per member
+    pf = pygrib.open(basedir + pfname)
+    cf = pygrib.open(basedir + cfname)
+    params = list(set([x.cfVarName for x in cf.select()])) # unique parameter values ["167.128","121.128","228.128"] # Hardcoded for marsParam
+    steprange = np.arange(0,1110,6) # Hardcoded
+    for hd in hdates:
+        collectparams = list()
+        for param in params:
+            collectsteps = list()
+            for step in steprange: 
+                # Get control values and lats, lons, units and longname of control
+                control = cf.select(cfVarName = param, validDate = pd.to_datetime(hd), stepRange = str(step))[0] # for hours use .stepRange I think.
+                controlval = control.values
+                lats,lons = control.latlons()
+                # get members values
+                members = pf.select(cfVarName = param, validDate = pd.to_datetime(hd), stepRange = str(step)) #
+                membersnum = [member.perturbationNumber for member in members] # 1 to 10, membernumers
+                membersval = [member.values for member in members]
+                # join both along the 'number' dimension by prepending the membernumbers (control = 0) and members values. Make xarray
+                membersnum.insert(0,0)
+                membersval.insert(0, controlval)
+                result = xr.DataArray(data = np.stack(membersval), 
+                                      coords=[membersnum, lats[:,0], lons[0,:]], 
+                                      dims=['number', 'latitude', 'longitude'])
+                collectsteps.append(result)
+            
+            # Combine along a time dimension, give parameter name 'test.cfVarName' and longname. Give units.
+            oneparam = xr.concat(collectsteps, 'time') 
+            collectparams.append(oneparam)
+
+        xr.Dataset(collectparams)
+        # Save the dataset to netcdf
+    pf.close()
+    cf.close()
+        
 
 def mars_dict(date, hdate = None, contr = False):
     """
@@ -97,11 +139,12 @@ def mars_dict(date, hdate = None, contr = False):
     'step'      : "0/to/1104/by/6",
     'area'      : "75/-30/25/75", #E-OBS 75.375/-40.375/25.375/75.375/ Limits 75/-40.5/25/75.5
     'grid'      : ".5/.5", # Octahedral grid does not support sub-areas
-    'format'    : "netcdf",
     'expect'    : "any",
     }
     if hdate is not None:
         req['hdate'] = hdate
+    else:
+        req['format'] = "netcdf"
     return(req)
         
 
@@ -109,6 +152,9 @@ def join_members(pfname, cfname):
     """
     Also seperate variables?
     """
+    pf = xr.open_dataset(basedir + pfname)
+    cf = xr.open_dataset(basedir + cfname)
+    cf.expand_dims('number')
     return()
 
 # TODO: Function for combining control and perturbed? De-accumulation of precipitation?
@@ -117,4 +163,4 @@ def join_members(pfname, cfname):
 # xr.diff()
     
 #start_batch(tmin = "2015-05-15", tmax = '2015-05-24')
-start_batch(tmin = '2015-05-24', tmax = '2015-05-29')
+#start_batch(tmin = '2015-05-24', tmax = '2015-05-29')
