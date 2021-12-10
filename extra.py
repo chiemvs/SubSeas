93d38d0ccf7c2954bcc007e869b3bf45e4615d97
chiemvs <chiemvs@gmail.com>
2021-12-10 16:08:32 +0000

    Generated a few new quantile climatologies with larger doy-windows

--- extra.py
+++ extra.py
@@ -15,36 +15,32 @@ if __name__ == "__main__":
     #t2m = SurfaceObservations(basevar = 'tg', basedir = '/nobackup/users/straaten/ERA5/', name = 't2m-anom_1979-01-01_2019-12-31_1D_0.25-degrees') # Override normal E-OBS directory
     #t2m.load(tmin = '1998-06-07', tmax = '2019-10-31')
     #t2m.aggregatespace(clustername = 't2m-q095-adapted', level = 15)
-    ###t2m.aggregatespace(clustername = 't2m-q075', level = 0.58)
     #t2m.aggregatetime(freq = '21D', method = 'mean', rolling = True)
-    ### write output. Correct the name for later matching to forecasts?
-    ### Utins match, namely K
+    ##### write output. Correct the name for later matching to forecasts?
+    ##### Utins match, namely K
     #t2m.newvar = 'anom' # Actually already done
     #t2m.construct_name(force = True) # Adds new tim/spacemethod
-    #print(t2m.name)
+    ##print(t2m.name)
     #t2m.array = t2m.array.drop(['nclusters','dissim_threshold'], errors = 'ignore')
-    ##out = xr.Dataset({'clustidfield':t2m.clusterarray.drop('nclusters', errors = 'ignore'),t2m.array.name:t2m.array})
-    ##out.to_netcdf(t2m.filepath)
+    ####out = xr.Dataset({'clustidfield':t2m.clusterarray.drop('nclusters', errors = 'ignore'),t2m.array.name:t2m.array})
+    ####out.to_netcdf(t2m.filepath)
     #t2m.clusterarray = t2m.clusterarray.drop(['nclusters','dissim_threshold'], errors = 'ignore') # So that they are not carried into matching
 
-    ## create a climatology
+    #### create a climatology
     #c = Climatology(f'{t2m.basevar}-{t2m.newvar}')
-    #c.localclim(obs = t2m, daysbefore = 5, daysafter = 5, mean = False, quant = 0.5)
+    #c.localclim(obs = t2m, daysbefore = 15, daysafter = 15, mean = False, quant = 0.9)
     #c.savelocalclim()
 
-    #t2m = SurfaceObservations(basevar = 'tg', basedir = '/nobackup/users/straaten/ERA5/', name = 'tg-anom_1998-06-07_2019-10-31_14D-roll-mean_15-t2m-q095-adapted-mean') # Override normal E-OBS directory
     #t2m = SurfaceObservations(basevar = 'tg', basedir = '/nobackup/users/straaten/ERA5/', name = 'tg-anom_1998-06-07_2019-10-31_31D-roll-mean_15-t2m-q095-adapted-mean') # Override normal E-OBS directory
-    #t2m = SurfaceObservations(basevar = 'tg', basedir = '/nobackup/users/straaten/ERA5/', name = 'tg-anom_1998-06-07_2019-10-31_21D-roll-mean_15-t2m-q095-adapted-mean') # Override normal E-OBS directory
-    #t2m = SurfaceObservations(basevar = 'tg', basedir = '/nobackup/users/straaten/ERA5/', name = 'tg-anom_1998-06-07_2019-10-31_7D-roll-mean_15-t2m-q095-adapted-mean') # Override normal E-OBS directory
-    #t2m = SurfaceObservations(basevar = 'tg', basedir = '/nobackup/users/straaten/ERA5/', name = 'tg-anom_1998-06-07_2019-10-31_7D-roll-mean_0.58-t2m-q075-mean') # Hihger spatial resolution
-    #t2m.load()
-    #t2m.newvar = 'anom'
+    t2m = SurfaceObservations(basevar = 'tg', basedir = '/nobackup/users/straaten/ERA5/', name = 'tg-anom_1998-06-07_2019-10-31_21D-roll-mean_15-t2m-q095-adapted-mean') # Override normal E-OBS directory
+    t2m.load()
+    t2m.newvar = 'anom'
 
     ### Matching. preparation with a highresmodelclim 
-    #highresmodelclim = ModelClimatology(cycle='45r1', variable = t2m.basevar, **{'name':'tg_45r1_1998-06-07_2019-08-31_1D_0.38-degrees_5_5_mean'}) # Name for loading
-    #highresmodelclim.local_clim()
-    #newvarkwargs={'climatology':highresmodelclim}
-    #loadkwargs = {'llcrnr':(30,None),'rucrnr':(None,42)} # Limiting the domain a bit.
+    highresmodelclim = ModelClimatology(cycle='45r1', variable = t2m.basevar, **{'name':'tg_45r1_1998-06-07_2019-08-31_1D_0.38-degrees_5_5_mean'}) # Name for loading
+    highresmodelclim.local_clim()
+    newvarkwargs={'climatology':highresmodelclim}
+    loadkwargs = {'llcrnr':(30,None),'rucrnr':(None,42)} # Limiting the domain a bit.
 
     #alignment = ForecastToObsAlignment(season = 'JJA', observations=t2m, cycle='45r1', n_members = 11, **{'expname':'paper3-1'}) # Season subsets the obs
     #alignment.match_and_write(newvariable = True, # Do I need loadkwargs
@@ -53,10 +49,10 @@ if __name__ == "__main__":
     #                          matchtime = True, 
     #                          matchspace= True)
 
-    #basedirkwargs = {'basedir':'/nobackup/users/straaten/EXT/'} # Needed because they need to be searching in EXT and not EXT_extra
-    #modelclim = ModelClimatology(cycle='45r1', variable = 'tg-anom') # Quantclimatology
-    #modelclim.local_clim(tmin = '1998-06-07', tmax = '2019-08-31', timemethod = t2m.timemethod, spacemethod = t2m.spacemethod, mean = False, quant = 0.5, lead_time_dep = True, clusterarray = t2m.clusterarray, loadkwargs = loadkwargs, newvarkwargs = newvarkwargs, basedirkwargs = basedirkwargs)
-    #modelclim.savelocalclim()
+    basedirkwargs = {'basedir':'/nobackup/users/straaten/EXT/'} # Needed because they need to be searching in EXT and not EXT_extra
+    modelclim = ModelClimatology(cycle='45r1', variable = 'tg-anom') # Quantclimatology
+    modelclim.local_clim(tmin = '1998-06-07', tmax = '2019-08-31', timemethod = t2m.timemethod, spacemethod = t2m.spacemethod, mean = False, quant = 0.9, lead_time_dep = True, daysbefore = 15, daysafter = 15, clusterarray = t2m.clusterarray, loadkwargs = loadkwargs, newvarkwargs = newvarkwargs, basedirkwargs = basedirkwargs)
+    modelclim.savelocalclim()
 
     """
     Creation of amount of hot days predictand
@@ -67,13 +63,13 @@ if __name__ == "__main__":
     #windowsize = 21
     #t2m = SurfaceObservations(basevar = 'tg', basedir = '/nobackup/users/straaten/ERA5/', name = 't2m-anom_1979-01-01_2019-12-31_1D_0.25-degrees') # Override normal E-OBS directory
     #t2m.load(tmin = '1998-06-07', tmax = '2019-10-31')
-    #t2m.aggregatespace(clustername = 't2m-grid', level = 0.01)
+    #t2m.aggregatespace(clustername = 't2m-q095-adapted', level = 15)
     #t2m.array = t2m.array.drop(['nclusters','dissim_threshold'], errors = 'ignore')
     #t2m.clusterarray = t2m.clusterarray.drop(['nclusters','dissim_threshold'], errors = 'ignore')
-    ##c = Climatology('t2m-anom')
-    ##c.localclim(obs = t2m, daysbefore = 5, daysafter = 5, mean = False, quant = 0.75)
-    ##c.clim.to_netcdf(c.filepath)
-    #c = Climatology('t2m-anom', name = 't2m-anom_clim_1998-06-07_2019-08-31_1D_15-t2m-q095-adapted-mean_5_5_q0.75')
+    ###c = Climatology('t2m-anom')
+    ###c.localclim(obs = t2m, daysbefore = 15, daysafter = 15, mean = False, quant = 0.75)
+    ###c.clim.to_netcdf(c.filepath)
+    #c = Climatology('t2m-anom', name = 't2m-anom_clim_1998-06-07_2019-10-31_1D_15-t2m-q095-adapted-mean_15_15_q0.75')
     #c.localclim()
     #
     #e = EventClassification(obs = t2m, quantclimatology = c, windowsize = windowsize)
@@ -83,7 +79,7 @@ if __name__ == "__main__":
     #out = xr.Dataset({'clustidfield':t2m.clusterarray.drop('nclusters', errors = 'ignore'),t2m.array.name:t2m.array})
     #out.to_netcdf(t2m.filepath)
 
-    #t2m = SurfaceObservations('tg',basedir = '/nobackup/users/straaten/ERA5/', name = 'tg-ex-q0.75-21D_1998-06-07_2019-10-31_1D_0.01-t2m-grid-mean')
+    #t2m = SurfaceObservations('tg',basedir = '/nobackup/users/straaten/ERA5/', name = 'tg-ex-q0.75-21D_1998-06-07_2019-10-31_1D_15-t2m-q095-adapted-mean')
     #t2m.load()
     #t2m.newvar = 'ex-q0.75-21D'
 
@@ -104,10 +100,10 @@ if __name__ == "__main__":
     #clusterarray = t2m.clusterarray
 
     #modelclim = ModelClimatology(cycle='45r1', variable = 'tg-anom') # Quantclimatology
-    #modelclim.local_clim(tmin = '1998-06-07', tmax = '2019-08-31', timemethod = '1D', spacemethod = t2m.spacemethod, mean = False, quant = 0.75, lead_time_dep = True, clusterarray = clusterarray, loadkwargs = loadkwargs, newvarkwargs = newvarkwargs, basedirkwargs = basedirkwargs)
+    #modelclim.local_clim(tmin = '1998-06-07', tmax = '2019-08-31', timemethod = '1D', spacemethod = t2m.spacemethod, mean = False, quant = 0.75, lead_time_dep = True, daysbefore = 15, daysafter = 15, clusterarray = clusterarray, loadkwargs = loadkwargs, newvarkwargs = newvarkwargs, basedirkwargs = basedirkwargs)
     #modelclim.savelocalclim()
 
-    #modelclim = ModelClimatology(cycle='45r1', variable = 'tg-anom', name = 'tg-anom_45r1_1998-06-07_2019-08-31_1D_15-t2m-q095-adapted-mean_5_5_q0.75')
+    #modelclim = ModelClimatology(cycle='45r1', variable = 'tg-anom', name = 'tg-anom_45r1_1998-06-07_2019-08-31_1D_15-t2m-q095-adapted-mean_15_15_q0.75')
     #modelclim.local_clim()
 
     # Now comes the matching.
